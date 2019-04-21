f9045bf59b9bc3f1b492340df4ad21548428772a
Lucas Navarro <lucasnavarro979@gmail.com>
2019-04-21 16:51:01 -0300

    purchasemail

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Youch = require('youch')
 const databaseConfig = require("./config/database.js");
 
 class App {
@@ -10,6 +11,7 @@ class App {
     this.database();
     this.middlewares();
     this.routes();
+    this.exception();
   }
 
   database() {
@@ -29,6 +31,24 @@ class App {
 
   routes() {
     this.express.use(require("./routes"));
+  }
+
+  exception() {
+    this.express.use(async (err, req, res, next) => {
+      if(err instanceof validate.ValidateEr) {
+        return res.status(err.status).json(err)
+      }
+
+      if(process.env.NODE_ENV !== 'production') {
+        const youch = new Youch(err)
+
+        return res.json(await youch.toJSON())
+      }
+
+      return res
+        .status(err.status || 500)
+        .json({ err: 'Internal Server Error' })
+    })
   }
 }
 
